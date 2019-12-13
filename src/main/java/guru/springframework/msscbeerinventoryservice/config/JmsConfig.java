84782424bade1c0d3a5461900bcb7e411821a057
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-12-13 06:48:29 +0000

    Add JMS Configuration. closes #5

---
+++ src/main/java/guru/springframework/msscbeerinventoryservice/config/JmsConfig.java
@@ -0,0 +1,24 @@
+package guru.springframework.msscbeerinventoryservice.config;
+
+/*
+ * Created by arunabhamidipati on 13/12/2019
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    public static final String inventoryQueue = "INVENTORY_QUEUE";
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
